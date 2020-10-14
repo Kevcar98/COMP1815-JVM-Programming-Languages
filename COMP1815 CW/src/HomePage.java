0e64e595e1e03072e1bf8aaf24af4b94eaa6fbe0
Kevcar98 <44064420+Kevcar98@users.noreply.github.com>
2020-10-14 15:26:43 +0100

    Add files via upload
    Started the GUI for the main menu, team creator and project creator

---
+++ COMP1815 CW/src/HomePage.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class HomePage {
+    private JButton viewTeamsButton;
+    private JButton createTeamButton;
+    private JButton createProjectButton;
+    private JButton viewProjectsButton;
+}
