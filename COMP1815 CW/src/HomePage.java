98c2cefd9dfe7d6c51d0ebc6872992ba1706359f
Kevcar98 <44064420+Kevcar98@users.noreply.github.com>
2020-10-15 13:18:27 +0100

    Add files via upload

--- COMP1815 CW/src/HomePage.java
+++ COMP1815 CW/src/HomePage.java
@@ -1,8 +1,53 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class HomePage {
     private JButton viewTeamsButton;
     private JButton createTeamButton;
     private JButton createProjectButton;
     private JButton viewProjectsButton;
+    private JPanel HomePanel;
+
+    public static void main(String[] args){
+        JFrame HomeP = new JFrame("HomePage");
+        HomeP.setContentPane(new HomePage().HomePanel);
+        HomeP.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        HomeP.pack();
+        HomeP.setVisible(true);
+
+    }
+
+    public HomePage() {
+        createTeamButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+
+
+
+            }
+        });
+        createProjectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        viewTeamsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        viewProjectsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
--- COMP1815 CW/src/TeamCreator.java
+++ COMP1815 CW/src/TeamCreator.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+
 public class TeamCreator {
+
+    private JButton backToMainMenuButton;
+    public JPanel TeamCPanel;
+
+    public static void main(String[] args){
+        JFrame TeamC = new JFrame("TeamCreator");
+        TeamC.setContentPane(new TeamCreator().TeamCPanel);
+        TeamC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        TeamC.pack();
+        TeamC.setVisible(true);
+
+    }
+
+
+
+    public TeamCreator() {
+    }
 }
