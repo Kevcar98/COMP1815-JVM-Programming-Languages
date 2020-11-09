43c1e4c81124876f9bf7e35d36bd15ed26907d00
Harry Di <47988198+hdrive99@users.noreply.github.com>
2020-11-09 16:54:51 +0000

    Add files via upload

--- creating tasks version/COMP1815 CW/src/ProjectCreator.java
+++ creating tasks version/COMP1815 CW/src/ProjectCreator.java
@@ -3,20 +3,36 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ProjectCreator {
     public JPanel ProjectCPanel;
     private JButton backToMainMenuButton;
     private JTextField AssignedTeamsF;
-    private JButton createProjectButton;
+    private JButton verifyProjectButton;
     private JTextField ProjectIDF;
     private JTextField CommissionerF;
     private JTextField ProjectManagerF;
     private JTextField NumOfTasksF;
     private JLabel ResultF;
-    private JButton saveProjectButton;
+    private JButton createProjectButton;
     private ProjectHandler handler;
     private List<Project> project;
+
+    boolean validationCheck(String s, boolean numberOnly) {
+        Pattern alpha = Pattern.compile("[^A-Za-z0-9 ]");
+        Pattern numbers = Pattern.compile("[^0-9]");
+        Matcher alphaMatcher = alpha.matcher(s);
+        Matcher numbersMatcher = numbers.matcher(s);
+        boolean alphaBoolean = alphaMatcher.find();
+        boolean numbersBoolean = numbersMatcher.find();
+        if ((alphaBoolean && !numberOnly) || (numbersBoolean && numberOnly)) {
+            System.out.println(s + " did not pass validation check");
+            return false;
+        }
+        return true;
+    }
 
     public ProjectCreator() {
         handler = new ProjectHandler();
@@ -36,11 +52,20 @@ public class ProjectCreator {
                 win.dispose();
             }
         });
-        createProjectButton.addActionListener(new ActionListener() {
+        verifyProjectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                project = handler.createProject(ProjectIDF.getText(), CommissionerF.getText(), ProjectManagerF.getText(), NumOfTasksF.getText(), AssignedTeamsF.getText());
-                ResultF.setText(project.get(project.size() - 1).toString()); // displays last item in list
+                if( validationCheck(ProjectIDF.getText(), true) &&
+                    validationCheck(CommissionerF.getText(), false) &&
+                    validationCheck(ProjectManagerF.getText(), false) &&
+                    validationCheck(NumOfTasksF.getText(), true) &&
+                    validationCheck(AssignedTeamsF.getText(), true)
+                ) {
+                    project = handler.createProject(ProjectIDF.getText(), CommissionerF.getText(), ProjectManagerF.getText(), NumOfTasksF.getText(), AssignedTeamsF.getText());
+                    ResultF.setText(project.get(project.size() - 1).toString()); // displays last item in list
+                } else {
+                    ResultF.setText("Error! Avoid using special characters or invalid inputs (e.g. letters in a text field expecting only numbers)");
+                }
                 // Resizes and centers current window by re-packing it
                 JComponent comp = (JComponent) e.getSource();
                 Window win = SwingUtilities.getWindowAncestor(comp);
@@ -48,7 +73,7 @@ public class ProjectCreator {
                 win.setLocationRelativeTo(null);
             }
         });
-        saveProjectButton.addActionListener(new ActionListener() {
+        createProjectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println(project.size());
